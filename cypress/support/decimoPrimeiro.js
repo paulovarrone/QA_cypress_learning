302574c728409fde621874501b1a350383458001
Paulo Varrone <paulorobertovarrone@gmail.com>
2026-05-15 00:35:27 -0300

    criando teste

---
+++ cypress/support/decimoPrimeiro.js
@@ -0,0 +1,17 @@
+Cypress.Commands.add('visitarURLdecimoPrim', () => {
+    cy.visit('http://automationexercise.com');
+    cy.get('#slider').should('be.visible');
+});
+
+Cypress.Commands.add('carrinhoCompras', () => {
+    cy.fixture('formulario').then((dados) => {
+        cy.visitarURLdecimoPrim();
+        cy.contains('a[href="/view_cart"]', 'Cart').click();
+        cy.scrollTo('bottom');
+        cy.contains('h2', 'Subscription').should('be.visible');
+        cy.get('#susbscribe_email').type(dados.Forms.email);
+        cy.get('#subscribe').click();
+        cy.contains('div', 'You have been successfully subscribed!').should('be.visible');
+    });
+    
+});
