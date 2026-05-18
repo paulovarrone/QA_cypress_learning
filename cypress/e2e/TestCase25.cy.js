3327ad8afac1ccdccc0cce6106a51f113ead899c
Paulo Varrone <paulorobertovarrone@gmail.com>
2026-05-18 11:33:28 -0300

    caso de teste 25

---
+++ cypress/e2e/TestCase25.cy.js
@@ -0,0 +1,24 @@
+describe('template spec', () => {
+ 
+    it('Entrar no site e ver se tem slider para se sertificar que é a home', () => {
+        cy.visitarURL26();
+    })
+
+    it('Scrollar para o fim da página', () => {
+        cy.scrollParaFimDaPagina();
+    })
+
+    it('Verificar se tem a seção de Subscription', () => {
+        cy.verificarSubscription();
+    })
+
+    it('Scrollar para o topo da página', () => {
+        cy.scrollParaTopoDaPagina();
+    })
+
+    it('Verificar se o scroll para o topo da página funcionou', () => {
+        cy.verificarScrollParaTopo();
+    })
+
+})
+
